b0f9b17c6c6628c216ec8caff9b9e1daf90a2f41
Danail Hadjiatanasov <hadjiatanasov@gmail.com>
2020-04-01 15:05:26 +0200

    fix article links

--- components/newsList/newsList.js
+++ components/newsList/newsList.js
@@ -2,12 +2,15 @@ import propTypes from 'prop-types';
 import './newsList.module.scss';
 
 const NewsList = props => {
+  const openArticle = url => {
+    const win = window.open(url, '_blank');
+    win.focus();
+  };
+
   return (
     <ul className="articles-container">
       { props.news.map((article, index) => (
-        <li key={index} className="article-item">
-          {/* <a href={article.url} className="article-link" target="_blank" rel="noopener">
-          </a>*/}
+        <li key={index} className="article-item" onClick={() => openArticle(article.url)}>
           <div className="article-text">
             <div className="article-title">
               {article.title}
