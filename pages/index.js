a971556f08ee5816de3c55bb2bd11afe507bae11
Teodora Jordanova <teodora.jordanovaa@gmail.com>
2020-04-01 09:58:09 +0200

    WIP

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import propTypes from 'prop-types';
 import { Fragment } from 'react/cjs/react.production.min';
 import Head from 'next/head';
-import SectionHeader from '../components/sectionHeader';
-import NewsList from '../components/newsList';
+import SectionHeader from '../components/sectionHeader/sectionHeader';
+import NewsList from '../components/newsList/newsList';
 import { getTopHeadlines, getSearchedHeadlines } from '../services/news';
-import SearchBar from '../components/searchBar';
+import SearchBar from '../components/searchBar/searchBar';
 
 const Home = props => {
   const [searchQuery, setSearchQuery] = useState(props.searchQuery);
@@ -22,7 +22,13 @@ const Home = props => {
     <Fragment>
       <Head>
         <title>The News Loader</title>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet" />
       </Head>
+      <style global jsx>{`
+       body {
+         font-family: 'Lato', sans-serif;
+       }
+       `}</style>
 
       <SearchBar searchNews={handleNewsSearch} />
 
